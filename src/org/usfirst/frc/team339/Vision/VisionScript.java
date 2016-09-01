c37d415e392d54c4a26476b3d9797ebe66a7324a
Kilroy Programming <Kilroy Programming@Kilroy-HP-2.mc.scps.k12.va.us>
2016-09-01 18:06:09 -0400

    Dirty workspace commit fo' reasons.

---
+++ src/org/usfirst/frc/team339/Vision/VisionScript.java
@@ -0,0 +1,17 @@
+package org.usfirst.frc.team339.Vision;
+
+import java.util.ArrayList;
+import org.usfirst.frc.team339.Vision.operators.NullOperator;
+import org.usfirst.frc.team339.Vision.operators.VisionOperatorInterface;
+
+public class VisionScript extends ArrayList<VisionOperatorInterface>
+{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public VisionScript ()
+	{
+	}
+}
